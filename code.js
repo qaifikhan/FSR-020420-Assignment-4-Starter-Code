1d3a420ba112348ba4455ffdc8dda3e9a11a2316
Qaifi Khan <qaifikhan@gmail.com>
2020-05-01 18:26:11 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,49 @@
+// Q.1
+function countWords(str) {
+  //Write your code here
+}
+
+// Q.2
+function frontBack3(str) {
+  //Write your code here
+}
+
+// Q.3
+function checkChar(str, char) {
+  //Write your code here
+}
+
+// Q.4
+function upperLower(str) {
+  //Write your code here
+}
+
+// Q.5
+function alternateSums(arr) {
+  //Write your code here
+}
+
+// Q.6
+function maxDifference(arr) {
+  //Write your code here
+}
+
+// Q.7
+function sumAdjacentDifference(arr) {
+  //Write your code here
+}
+
+// Q.8
+function calcAvAgeRef(obj) {
+  //Write your code here
+}
+
+// Q.9
+function compareObjects(obj1, obj2) {
+  //Write your code here
+}
+
+// Q.10
+function frequency(arr) {
+  //Write your code here
+}
